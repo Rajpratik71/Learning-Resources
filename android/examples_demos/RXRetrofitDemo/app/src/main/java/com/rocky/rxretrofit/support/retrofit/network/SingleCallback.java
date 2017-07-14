469e96c0d00c2dfd490ec8c8271802453b28cfdd
raghav <raghavsatyadev@gmail.com>
2017-07-14 10:26:26 +0530

    - Added RXRetrofitDemo

--- android/examples_demos/RXRetrofitDemo/app/src/main/java/com/rocky/rxretrofit/support/retrofit/network/SingleCallback.java
+++ android/examples_demos/RXRetrofitDemo/app/src/main/java/com/rocky/rxretrofit/support/retrofit/network/SingleCallback.java
@@ -2,7 +2,7 @@ package com.rocky.rxretrofit.support.retrofit.network;
 
 public interface SingleCallback {
     /**
-     * @param o        Whole response Object
+     * @param o        Whole response Object.
      * @param apiNames {@link com.rocky.rxretrofit.support.retrofit.network.WebserviceBuilder.ApiNames} to differentiate Apis
      */
     void onSingleSuccess(Object o, WebserviceBuilder.ApiNames apiNames);
